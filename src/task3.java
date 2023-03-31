913dc87cf56cfb2b34fee83702f987dd66b5fd9c
Roman <romankmet10@gmail.com>
2023-03-31 10:19:01 +0300

    Lab2

--- src/task3.java
+++ src/task3.java
@@ -1,17 +1,20 @@
 public class task3 {
     public static void main(String[] args) {
         int floor=5;
-        String direction="Вниз";
+        String direction="Вверх";
         if(floor==2&&direction=="Вверх"){
             floor=3;
-        }else if (floor==2&&direction=="Вниз"){
+        }else if(floor==2&&direction=="Вниз"){
             floor=1;
         }
         if(direction=="Вниз"&&floor<9){
             System.out.println("Ви спустились на "+floor+" поверх");
         }
-        else{
+        else if(direction=="Вверх"&&floor>1){
             System.out.println("Ви піднялись на "+floor+" поверх");
+        }
+        else{
+            System.out.println("Такого поверху не існує");
         }
     }
 }
