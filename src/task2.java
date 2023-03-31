ed1a5f655250d7ddf189b82ca7c2ccf9d40c8905
Roman <romankmet10@gmail.com>
2023-03-31 10:12:50 +0300

    lab2

--- src/task2.java
+++ src/task2.java
@@ -1,16 +1,15 @@
 public class task2 {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 5;
-        int c = 7;
-        if(a>b)
-            if(a>c)
-                System.out.println(a);
-            else {
-                if(b>c)
-                    System.out.println(b);
-            }else {
-            System.out.println(c);
+        int t = 752;
+        int a = t / 100;
+        int b = (t % 100) / 10;
+        int c = t % 10;
+        if (a >= b && a >= c) {
+            System.out.println("Перше число найбільше");
+        } else if (b >= a && b >= c) {
+            System.out.println("Друге число найбільше");
+        } else {
+            System.out.println("Третє число найбільше");
         }
     }
 }
--- src/task3.java
+++ src/task3.java
@@ -1,26 +1,17 @@
 public class task3 {
     public static void main(String[] args) {
-        int level = 2;
-        if(level==1) {
-            System.out.println("Ви піднялись на 1 поверх");
-        }else if(level==2){
-            System.out.println("Ви піднялись на 3 поверх");
-        }else if(level==3){
-            System.out.println("Ви піднялись на 3 поверх");
-        }else if(level==4){
-            System.out.println("Ви піднялись на 4 поверх");
-        }else if(level==5){
-            System.out.println("Ви піднялись на 5 поверх");
-        }else if(level==6){
-            System.out.println("Ви піднялись на 6 поверх");
-        }else if(level==7){
-            System.out.println("Ви піднялись на 7 поверх");
-        }else if(level==8){
-            System.out.println("Ви піднялись на 8 поверх");
-        }else if(level==9){
-            System.out.println("Ви піднялись на 9 поверх");
-        }else{
-            System.out.println("Такого поверху не існує");
+        int floor=5;
+        String direction="Вниз";
+        if(floor==2&&direction=="Вверх"){
+            floor=3;
+        }else if (floor==2&&direction=="Вниз"){
+            floor=1;
+        }
+        if(direction=="Вниз"&&floor<9){
+            System.out.println("Ви спустились на "+floor+" поверх");
+        }
+        else{
+            System.out.println("Ви піднялись на "+floor+" поверх");
         }
     }
 }
