07b0209cc416b1c46258b9f6d56942638965a944
Roman <romankmet10@gmail.com>
2023-04-13 22:41:17 +0300

    labboratorna3

--- src/task3.java
+++ src/task3.java
@@ -1,19 +1,18 @@
 public class task3 {
     public static void main(String[] args) {
-        int floor=5;
-        String direction="Вверх";
-        if(floor==2&&direction=="Вверх"){
-            floor=3;
-        }else if(floor==2&&direction=="Вниз"){
-            floor=1;
-        }
-        if(direction=="Вниз"&&floor<9){
-            System.out.println("Ви спустились на "+floor+" поверх");
-        }
-        else if(direction=="Вверх"&&floor>1){
-            System.out.println("Ви піднялись на "+floor+" поверх");
-        }
-        else{
+        String direction = "Up";
+        String s1 = "Up";
+        String s2 = "Down";
+        int level = 4;
+        if (direction.equals(s2) && level == 2){
+            level=1;
+        }if (direction.equals(s1)&& level == 2) {
+            level=3;
+        }if (direction.equals(s2) && level < 9){
+            System.out.println("Ви спустились на " + level + " поверх");
+        }else if(direction.equals(s1) && level < 9){
+            System.out.println("Ви піднялись на " + level + " поверх");
+        }else{
             System.out.println("Такого поверху не існує");
         }
     }
